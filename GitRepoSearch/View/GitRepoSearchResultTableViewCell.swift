6a3c7d1424dcf6df4b41cd964c32c1ddd6d3d6ad
eleven <y.osawa11@gmail.com>
2022-11-17 14:30:27 +0900

    ViewとViewModelを追加

---
+++ GitRepoSearch/View/GitRepoSearchResultTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  GitRepoSearchResultTableViewCell.swift
+//  GitRepoSearch
+//
+//  Created by eleven on 2022/11/17.
+//
+
+import UIKit
+
+final class GitRepoSearchResultTableViewCell: UITableViewCell {
+}
---
+++ GitRepoSearch/ViewModel/GitRepoSearchViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  GitRepoSearchViewModel.swift
+//  GitRepoSearch
+//
+//  Created by eleven on 2022/11/17.
+//
+
+import Foundation
+
+final class GitRepoSearchViewModel {
+}
